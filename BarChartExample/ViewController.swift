9cafaf9d313d3b4fb517d6bbf7b5f3bf3732d09d
VuslatYolcu <ylcvalt@gmail.com>
2024-09-21 21:34:49 +0300

    Edit bar chart according to the assets page

--- BarChartExample/ViewController.swift
+++ BarChartExample/ViewController.swift
@@ -16,28 +16,41 @@ class ViewController: UIViewController {
         
         barChart.zoom(scaleX: 2, scaleY: 1, x: 0, y: 0)
         barChart.legend.enabled = false
-        
-        // Configure the xAxis to be enabled and visible
-        barChart.xAxis.enabled = true
-        barChart.xAxis.drawAxisLineEnabled = true
-        barChart.xAxis.drawGridLinesEnabled = false
-        barChart.xAxis.labelTextColor = .white
-        barChart.xAxis.labelPosition = .bottom
-        
-        barChart.leftAxis.enabled = true
-        barChart.leftAxis.labelTextColor = .white
-        
-        barChart.rightAxis.enabled = false
-        
-        barChart.legend.enabled = false
         barChart.chartDescription.enabled = false
         barChart.gridBackgroundColor = .clear
-    
         barChart.dragEnabled = true
-        barChart.setScaleEnabled(false) // Disable pinch zooming
-        barChart.pinchZoomEnabled = false
+        barChart.scaleXEnabled = false
+        barChart.scaleYEnabled = false
         barChart.doubleTapToZoomEnabled = false
-    
+        barChart.pinchZoomEnabled = false
+        barChart.drawGridBackgroundEnabled = false
+        barChart.chartDescription.text = ""
+        
+        // x axis
+        let xAxis = barChart.xAxis
+        xAxis.enabled = true
+        xAxis.drawAxisLineEnabled = false
+        xAxis.drawGridLinesEnabled = false
+        xAxis.labelTextColor = .white
+        xAxis.labelPosition = .bottom
+        xAxis.axisMaximum = -0.45
+        xAxis.granularityEnabled = true
+        xAxis.granularity = 1
+        
+        // right axis
+        let rightAxis = barChart.rightAxis
+        rightAxis.enabled = false
+        rightAxis.drawLabelsEnabled = false
+        rightAxis.spaceBottom = 0.0
+        
+        // left axis
+        let leftAxis = barChart.leftAxis
+        leftAxis.enabled = true
+        leftAxis.labelTextColor = .white
+        leftAxis.drawGridLinesEnabled = true
+        leftAxis.spaceBottom = 0.0
+        leftAxis.axisMinimum = 0
+        
         return barChart
     }()
     
@@ -54,6 +67,7 @@ class ViewController: UIViewController {
         
         // Supply data to the chart
         supplyData()
+        barChart.moveViewToAnimated(xValue: 12, yValue: 0, axis: .left, duration: 3)
     }
 
     private func supplyData() {
@@ -72,8 +86,8 @@ class ViewController: UIViewController {
         let data = BarChartData(dataSet: set)
         
         barChart.data = data
-        
-        
+        barChart.barData?.setDrawValues(true)
+        barChart.xAxis.axisMaximum = data.xMax + 0.45
     }
 }
 
